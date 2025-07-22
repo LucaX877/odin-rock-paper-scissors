e5dd0083e37fb5fd0b8fc40bafa9fd2167f76c71
Luca <171629213+LucaX877@users.noreply.github.com>
2025-07-22 23:32:11 +0100

    Create function to play round

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 
 function getComputerChoice() {
     let num = Math.floor(Math.random()*3);
-    if (num === 0) return "Rock";
-    else if (num === 1) return "Paper";
-    else return "Scissors";
+    if (num === 0) return "rock";
+    else if (num === 1) return "paper";
+    else return "scissors";
     }
 
 function getHumanChoice() {
@@ -12,9 +12,18 @@ function getHumanChoice() {
     }
 
 function playRound(computerChoice,humanChoice) {
-    return;
+    humanChoice = humanChoice.toLowerCase();
+    console.log(computerChoice,humanChoice);
+    if (computerChoice === humanChoice) console.log("You drew!");
+    else if (computerChoice === "rock" && humanChoice === "paper") console.log("You win! Paper beats rock.");
+    else if (computerChoice === "rock" && humanChoice === "scissors") console.log("You lose! Rock beats scissors.");
+    else if (computerChoice === "paper" && humanChoice === "rock") console.log("You lose! Paper beats rock.");
+    else if (computerChoice === "paper" && humanChoice === "scissors") console.log("You win! Scissors beats paper.");
+    else if (computerChoice === "scissors" && humanChoice === "paper") console.log("You lose! Scissors beats paper.");
+    else console.log("You win! Rock beats scissors.");
     }
 
 let computerScore = 0, humanScore = 0;
 let computerChoice = getComputerChoice();
 let humanChoice = getHumanChoice();
+playRound(computerChoice,humanChoice);
